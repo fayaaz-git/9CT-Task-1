6f1bc57de5bf7aafbdf80be1a0efd1304cc725fa
fayaaz-git <fayaaz.kabir841@education.nsw.gov.au>
2026-05-15 12:29:49 +1000

    Oh my days, I'm not screwed anymore!!!!
    Finally added all my data CSVs, and completed the written dataset section of my interface. Just need to do the visuals (which should be easy).

--- data_module.py
+++ data_module.py
@@ -6,6 +6,14 @@ comparison_df = pd.read_csv(
                             "data/phone_and_book_comparison.csv",
                             header = None)
 
+last_book_df = pd.read_csv(
+                        "data/last_book_time.csv",
+                        header = None)
+
+reasons_df = pd.read_csv(
+                        "data/subject_reasons.csv",
+                        header = None)
+
 complete_df = pd.read_csv(
                         "data/phone_and_book.csv",
                         header = None)
@@ -15,7 +23,14 @@ complete_df = complete_df.iloc[:,[1,2,3,4]]
 def written_complete():
     print(complete_df)
 
+def written_last():
+    print(last_book_df)
+
+def reasons():
+    print(reasons_df)
+
 def written_comparison():
     print(comparison_df)
 
-written_complete()
+def visual_comparison():
+    plt.plot(comparison_df["Amount people who spend this amount of time on..."], comparison_df["Their phone (daily)"])
--- main.py
+++ main.py
@@ -1,11 +1,56 @@
-import os
-import pandas as pd
-import matplotlib.pyplot as plt
-from data_module import written_comparison, comparison_df
+import os                                                                                                   # Imports OS
+import pandas as pd                                                                                         # Imports Pandas (should be in data module but just in case)
+import matplotlib.pyplot as plt                                                                             # Imports Matplotlib (should be in data module but just in case)
+from data_module import *      # Imports everything from data module
 
 
 def cls():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')                                                        # Makes a function to clear everything in terminal if triggered
+
+def dataset():                                                                                              # Function that allows specific datasets to be displayed in certain ways if triggered
+    print("DATASET VIEWER\n")
+
+    print("1. Show the dataset")
+    print("2. Show a visual representation of the data\n")
+
+    rep_choice = input("Please choose one of the following options (1-2).\n")
+
+    if rep_choice == "1":
+        cls()
+        print("DATASET VIEWER\n")
+
+        print("Choose your dataset:")
+        print("1. Comparison Between Daily Phone and Weekly Book Usage")
+        print("2. Last Time Subjects Read Books")
+        print("3. Most Common Reasons for Not Reading Books")
+        print("4. The Entire Survey Dataset\n")
+
+        data_choice = input("Please choose one of the following options (1-4).\n")
+
+        if data_choice == "1":
+            cls()
+            written_comparison()
+            input("Press enter to continue...")
+        
+        elif data_choice == "2":
+            cls()
+            written_last()
+            input("Press enter to continue")
+
+        elif data_choice == "3":
+            cls()
+            reasons()
+            input("Press enter to continue...")
+        
+        elif data_choice == "4":
+            cls()
+            written_complete()
+            input("Press enter to continue")
+        
+        else:
+            cls()
+            print("ERROR!\n")
+            input("Press enter to continue...")
 
 def interface():
     while True:
@@ -14,15 +59,13 @@ def interface():
         print("1. View a dataset")
         print("2. Exit the program")
         print("3. Edit specific data [AUTHORISED PERSONEL ONLY!]")
-        print("4. Save changes [AUTHORISED PERSONEL ONLY!]")
-        print()
+        print("4. Save changes [AUTHORISED PERSONEL ONLY!]\n")
 
-        choice = input("Please choose one of the following options (1-4), please.\n")
+        choice = input("Please choose one of the following options (1-4).\n")
 
         if choice == "1":
             cls()
-            print("Dataset\n")
-            input("Press enter to continue, please.")
+            dataset()
             cls()
             interface()
         
@@ -48,4 +91,5 @@ def interface():
             input("Press enter to continue, please.")
             cls()
 
+cls()
 interface()
